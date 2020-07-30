c47a1bb516155d938f991baf64b36f523b627feb
Susmit Vengurlekar <susmit.py@gmail.com>
2020-07-30 17:47:53 +0530

    Update settings.py

--- src/AMS/settings.py
+++ src/AMS/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')8306bn)*_o@ajz(51*-*a3(scg*nt^)tfhjzital+7!e$gm#v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["web"]
 
 
 # Application definition
